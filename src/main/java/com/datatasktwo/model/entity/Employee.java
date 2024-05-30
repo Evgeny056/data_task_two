66865467aac89051b004b0bf5f4329e0d1818a7d
Evgeny Shurupov <schuevgeny@yandex.ru>
2024-05-30 16:28:06 +0300

    Added entity

---
+++ src/main/java/com/datatasktwo/model/entity/Employee.java
@@ -0,0 +1,33 @@
+package com.datatasktwo.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String position;
+    private Double salary;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    @JsonBackReference
+    private Department department;
+}
